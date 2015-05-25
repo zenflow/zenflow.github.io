fff2444a1280c710db1ea29034a592140be59926
Matthew Francis Brunetti <zenflow87@gmail.com>
2015-05-25 18:13:53 -0400

    added template.html with google analytics script

--- gulpfile.js
+++ gulpfile.js
@@ -10,9 +10,12 @@ gulp.task('clean', function(cb){
 gulp.task('build', ['clean'], function(cb){
     fs.readFile(path.join(__dirname, 'README.md'), 'utf8', function(error, markdown_in){
         if (error){return cb(error);}
-        var html_out = markdown.parse(markdown_in);
-        console.log(html_out)
-        fs.writeFile(path.join(__dirname, 'index.html'), html_out, cb);
+        var readme_html = markdown.parse(markdown_in);
+        fs.readFile(path.join(__dirname, 'template.html'), 'utf8', function(error, html_in){
+            if (error){return cb(error);}
+            var index_html = html_in.replace(/{{markdown}}/, readme_html);
+            fs.writeFile(path.join(__dirname, 'index.html'), index_html, cb);
+        });
     });
 });
 gulp.task('default', ['build']);
